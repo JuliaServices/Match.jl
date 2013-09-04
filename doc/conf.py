359206a204da90a65607e4caf6ee4702cd5f2583
Kevin Squire <kevin.squire@gmail.com>
2013-09-03 21:47:18 -0700

    Fixes for ReadTheDocs/sphinx

--- doc/conf.py
+++ doc/conf.py
@@ -12,6 +12,7 @@
 # serve to show the default.
 
 import sys, os
+import juliadoc
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
@@ -25,7 +26,7 @@ import sys, os
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = []
+extensions = ['juliadoc.julia', 'juliadoc.jlhelp']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = []
