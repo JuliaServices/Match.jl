97f73ccd58b1c33c129c4b46af6ab74ab9c90225
Kevin Squire <kevin@secondspectrum.com>
2014-05-26 14:02:52 -0700

    Julia v0.2 compatibility: deleteat! was not defined in v0.2

--- src/Match.jl
+++ src/Match.jl
@@ -4,6 +4,12 @@ using Base.Meta
 
 export @match, @ismatch
 
+# Julia 0.2 compatibility patch
+if isless(Base.VERSION, v"0.3.0-")
+    deleteat! = splice!
+end
+
+
 include("matchutils.jl")
 include("matchmacro.jl")
 
