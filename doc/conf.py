4cf429650df0df1ef9c07ce7ac0d1e5a523a30ae
Kevin Squire <kevin@secondspectrum.com>
2016-01-12 17:21:28 -0800

    Fix RTD documentation generation
    * At some point recently, RTD stopped being able to find the
      sphinx_rtd_theme.  The fix is copied from DataFrames.jl.
    
    (cherry picked from commit 7b28ce1d562b56c1216a07bbfd465f86d015f46d)

--- doc/conf.py
+++ doc/conf.py
@@ -13,6 +13,7 @@
 
 import sys, os
 import juliadoc
+import sphinx_rtd_theme
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
@@ -103,7 +104,8 @@ html_theme = 'julia'
 #html_theme_options = {}
 
 # Add any paths that contain custom themes here, relative to this directory.
-html_theme_path = [juliadoc.get_theme_dir()]
+html_theme_path = [juliadoc.get_theme_dir(),
+                   sphinx_rtd_theme.get_html_theme_path()]
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
